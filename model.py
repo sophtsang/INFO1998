3635ab2bdda7ee74f918b87aa33728f0b0bd78b6
angelatnbao <127065305+angelatnbao@users.noreply.github.com>
2024-05-01 22:56:18 -0400

    Update model.py

--- model.py
+++ model.py
@@ -6,19 +6,20 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt 
 
 class Model(object): 
-    def linearModel(self, tax, lastSold, bedrooms, df1): 
+    def linearModel(self, tax, salePrice, bedrooms, df1): 
         df = df1.copy() 
-        df = df.dropna(axis=0, subset = ['tax paid', 'last sold price', 'bedrooms', 'sold price'])
+        df = df.dropna(axis=0, subset = ['tax paid', 'sale price', 'bedrooms', 'sold price'])
 
-        features = df[['tax paid', 'last sold price', 'bedrooms']] 
+        features = df[['tax paid', 'sale price', 'bedrooms']] 
         target = df['sold price'] 
         x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
         model = LinearRegression() 
         model.fit(x_train, y_train) 
         
-        predicted = model.predict([[tax, lastSold, bedrooms]])[0] 
+        predicted = model.predict([[tax, salePrice, bedrooms]])[0] 
         trainScore = model.score(x_train,y_train) 
         validation = model.score(x_test,y_test) 
         
         values = [predicted, trainScore, validation] 
         return values 
+
